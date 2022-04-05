659eb5c981086e9b7f4e451ddd30e4dbf084e7af
Mehdi Saber <m1d3s@protonmail.com>
2022-04-05 15:58:02 +0200

    WIP: tests

--- tests/Unit/ToDoListTest.php
+++ tests/Unit/ToDoListTest.php
@@ -4,14 +4,14 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+class ToDoListTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function test_that_true_is_true()
+    public function testDeleteCascade()
     {
         $this->assertTrue(true);
     }
